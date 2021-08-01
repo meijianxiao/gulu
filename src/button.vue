147a7e22c96878b4a0cb2c988607a1dd2db528b3
meijianxiao <1003409251@qq.com>
2021-08-01 16:16:30 +0800

    添加菊花状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
     <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <span class="content">
       <slot></slot>
     </span>
@@ -23,6 +24,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@keyframes spin {
+  0%{
+    transform: rotate(0deg);}
+  100%{
+    transform: rotate(360deg);}
+}
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -67,6 +74,9 @@ export default {
     > .content {
       order: 1;
     }
+  }
+  .loading{
+    animation: spin 2s infinite linear;
   }
 }
 
