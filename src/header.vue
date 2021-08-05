50ffd6bedddab60f7b2915b2568296fc70795140
meijianxiao <1003409251@qq.com>
2021-08-05 16:27:54 +0800

    添加简单动画

---
+++ src/header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+    <slot></slot>
+  </div>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style lang="scss" scoped>
+.header{
+  background: #999;
+  height: 100px;
+}
+</style>
